86af5ec283268437819724be26f6be6846f6982f
RicardoGHa <ricardoghattas112@gmail.com>
2024-10-15 22:39:21 +0300

    data integration

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-from src.WineQualityProject.logging import logger
+from WineQualityProject import logger
+from WineQualityProject.pipeline.stage_01_data_ingestion import (
+    DataIngestionTrainingPipeline,
+)
 
-logger.info("This is our custom log!")
+STAGE_NAME = "Data Ingestion stage"
+
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
