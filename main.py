dac8f111261ab01f11fcf3b1de913a735a8dd106
RicardoGHa <ricardoghattas112@gmail.com>
2024-10-15 23:28:02 +0300

    data transformation

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from WineQualityProject import logger
-from WineQualityProject.pipeline.stage_02_data_validation import (
-    DataValidationTrainingPipeline,
+from WineQualityProject.pipeline.stage_03_data_transformation import (
+    DataTransformationTrainingPipeline,
 )
 
 
-STAGE_NAME = "Data Validation stage"
+STAGE_NAME = "Data Transformation stage"
 
 
 if __name__ == "__main__":
     try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        obj = DataValidationTrainingPipeline()
+        obj = DataTransformationTrainingPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
