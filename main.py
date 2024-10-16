9011c6adb2fea29f279fc9f279038c7b6d419e10
RicardoGHa <ricardoghattas112@gmail.com>
2024-10-16 13:30:40 +0300

    built the model and evaluated it

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 from WineQualityProject import logger
-from WineQualityProject.pipeline.stage_03_data_transformation import (
-    DataTransformationTrainingPipeline,
+from WineQualityProject.pipeline.stage_04_model_trainer import (
+    ModelTrainerTrainingPipeline,
+)
+from WineQualityProject.pipeline.stage_05_model_evaluation import (
+    ModelEvaluationTrainingPipeline,
 )
 
-
-STAGE_NAME = "Data Transformation stage"
+STAGE_NAME = "Model Trainer stage"
 
 
 if __name__ == "__main__":
     try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        obj = DataTransformationTrainingPipeline()
+        obj = ModelTrainerTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Model evaluation stage"
+
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelEvaluationTrainingPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
